575f11ed2e13ab7890239ec2cb859230582da035
mkrecny <mkrecny@gmail.com>
2012-02-17 11:37:50 -0500

    restricting numbers

--- tests/build.test.js
+++ tests/build.test.js
@@ -2,8 +2,10 @@ var db_conf = require('../conf/db.js');
 var mongoat = require('mongoat')(db_conf.db_name, db_conf.host, db_conf.port);
 var build = require('../lib/build.js');
 var build_num_users = 1000;
+var fs = require('fs');
 
+var num_users = process.argv[2] || 'all';  
 mongoat.db.open(function(e, db_client){
-  build(db_client, build_num_users, mongoat.do_query, function(e, graph){
+  build(db_client, num_users, mongoat.do_query, function(e, graph){
   });
 });
